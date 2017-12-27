24b8dcf78975cd7f92ba04c365d060ca1a7e8864
Zaxophone <i.zack.how.e@gmail.com>
2017-12-27 12:17:03 -0800

    Uploaded everything

---
+++ Test/Browser.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace ZacksBrowser
+{
+    partial class Browser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Browser));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setPageAsHomeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.windowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newWindowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.themesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.darkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lightToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.browserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewOnGithubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 56);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(999, 632);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.WebBrowser1_DocumentCompleted);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(156, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "http://";
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(412, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(72, 27);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(37, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "<-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(115, 27);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(35, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "->";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 27);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(54, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Home";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.windowToolStripMenuItem,
+            this.browserToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1023, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.setPageAsHomeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
+            // 
+            // setPageAsHomeToolStripMenuItem
+            // 
+            this.setPageAsHomeToolStripMenuItem.Name = "setPageAsHomeToolStripMenuItem";
+            this.setPageAsHomeToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.setPageAsHomeToolStripMenuItem.Text = "Set Page as Home";
+            this.setPageAsHomeToolStripMenuItem.Click += new System.EventHandler(this.SetPageAsHomeToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.exitToolStripMenuItem.Text = "Exit (Alt+F4)";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // windowToolStripMenuItem
+            // 
+            this.windowToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newWindowToolStripMenuItem,
+            this.themesToolStripMenuItem});
+            this.windowToolStripMenuItem.Name = "windowToolStripMenuItem";
+            this.windowToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.windowToolStripMenuItem.Text = "Window";
+            // 
+            // newWindowToolStripMenuItem
+            // 
+            this.newWindowToolStripMenuItem.Name = "newWindowToolStripMenuItem";
+            this.newWindowToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newWindowToolStripMenuItem.Text = "New Window";
+            this.newWindowToolStripMenuItem.Click += new System.EventHandler(this.NewWindowToolStripMenuItem_Click);
+            // 
+            // themesToolStripMenuItem
+            // 
+            this.themesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.darkToolStripMenuItem,
+            this.lightToolStripMenuItem});
+            this.themesToolStripMenuItem.Name = "themesToolStripMenuItem";
+            this.themesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.themesToolStripMenuItem.Text = "Themes";
+            // 
+            // darkToolStripMenuItem
+            // 
+            this.darkToolStripMenuItem.Name = "darkToolStripMenuItem";
+            this.darkToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.darkToolStripMenuItem.Text = "Dark";
+            this.darkToolStripMenuItem.Click += new System.EventHandler(this.DarkToolStripMenuItem_Click);
+            // 
+            // lightToolStripMenuItem
+            // 
+            this.lightToolStripMenuItem.Name = "lightToolStripMenuItem";
+            this.lightToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.lightToolStripMenuItem.Text = "Light";
+            this.lightToolStripMenuItem.Click += new System.EventHandler(this.LightToolStripMenuItem_Click);
+            // 
+            // browserToolStripMenuItem
+            // 
+            this.browserToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewOnGithubToolStripMenuItem});
+            this.browserToolStripMenuItem.Name = "browserToolStripMenuItem";
+            this.browserToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.browserToolStripMenuItem.Text = "Browser";
+            // 
+            // viewOnGithubToolStripMenuItem
+            // 
+            this.viewOnGithubToolStripMenuItem.Name = "viewOnGithubToolStripMenuItem";
+            this.viewOnGithubToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.viewOnGithubToolStripMenuItem.Text = "Check for Updates";
+            this.viewOnGithubToolStripMenuItem.Click += new System.EventHandler(this.ViewOnGithubToolStripMenuItem_Click);
+            // 
+            // Browser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 700);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Browser";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Zack\'s Browser";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setPageAsHomeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem windowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newWindowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem themesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem darkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lightToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem browserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewOnGithubToolStripMenuItem;
+    }
+}
+
---
+++ Test/Browser.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ZacksBrowser
+{
+    public partial class Browser : Form
+    {
+        public Browser() //Startup code
+        {
+            InitializeComponent();
+            string HomeDir = (Directory.GetCurrentDirectory() + "\\" + "Homepage.txt");
+            string AboutDir = File.ReadAllText(HomeDir);
+            webBrowser1.Navigate(AboutDir);
+            LightTheme();
+        }
+
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e) //When the browser finishes loading a page do this
+        {
+            textBox1.Text = webBrowser1.Url.ToString();
+            Text = textBox1.Text;
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e) //When the user changes the URL text do this
+        {
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e) //Go button
+        {
+            if (textBox1.Text == null)
+            {
+                string WkDir = Directory.GetCurrentDirectory();
+                string HpDir = (WkDir + "\\" + "Homepage.txt");
+                webBrowser1.Navigate(HpDir);
+            }
+            else
+            {
+                webBrowser1.Navigate(textBox1.Text);
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e) //Back button
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void Button4_Click(object sender, EventArgs e) //Forward button
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void Button5_Click(object sender, EventArgs e) //Home button
+        {
+            string HomeDir = (Directory.GetCurrentDirectory() + "\\" + "Homepage.txt");
+            string AboutDir = File.ReadAllText(HomeDir);
+            webBrowser1.Navigate(AboutDir);
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //File-About menu item
+            string AboutDir = Directory.GetCurrentDirectory() + "\\" + "About.html";
+            webBrowser1.Navigate(AboutDir);
+        }
+
+        private void SetPageAsHomeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //File-Set Page as Home menu item
+            string HomeDir = (Directory.GetCurrentDirectory() + "\\" + "Homepage.txt");
+            System.IO.File.WriteAllText(HomeDir, textBox1.Text);
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //File-Exit menu item
+            this.Text = ("Goodbye!");
+            Thread.Sleep(900);
+            Application.Exit();
+        }
+
+        private void DarkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Window-Themes-Dark menu item
+            DarkTheme();
+        }
+
+        private void LightToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Window-Themes-Light menu item
+            LightTheme();
+        }
+
+        private void NewWindowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Window-New Window menu item
+            string wd = Directory.GetCurrentDirectory();
+            string exe = (wd + "\\" + "ZacksBrowser.exe");
+            Process.Start(exe);
+        }
+
+        private void ViewOnGithubToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Browser-View on GitHub menu item
+            webBrowser1.Navigate("http://www.github.com/zaxophone/ZacksBrowser");
+        }
+
+        public void LightTheme()
+        {
+            //Light theme
+            BackColor = Color.FromArgb(250, 235, 235);
+            button5.BackColor = Color.FromArgb(245, 245, 245);
+            button5.ForeColor = Color.FromArgb(0, 0, 0);
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.FlatAppearance.BorderColor = Color.FromArgb(254, 245, 245);
+            button5.FlatAppearance.BorderSize = 1;
+            button4.BackColor = Color.FromArgb(245, 245, 245);
+            button4.ForeColor = Color.FromArgb(0, 0, 0);
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.FlatAppearance.BorderColor = Color.FromArgb(254, 245, 245);
+            button4.FlatAppearance.BorderSize = 1;
+            button3.BackColor = Color.FromArgb(245, 245, 245);
+            button3.ForeColor = Color.FromArgb(0, 0, 0);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.FlatAppearance.BorderColor = Color.FromArgb(254, 245, 245);
+            button3.FlatAppearance.BorderSize = 1;
+            button1.BackColor = Color.FromArgb(245, 245, 245);
+            button1.ForeColor = Color.FromArgb(0, 0, 0);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.FlatAppearance.BorderColor = Color.FromArgb(254, 245, 245);
+            button1.FlatAppearance.BorderSize = 1;
+            textBox1.BackColor = Color.FromArgb(254, 245, 245);
+            textBox1.ForeColor = Color.FromArgb(0, 0, 0);
+            menuStrip1.BackColor = Color.FromArgb(199, 199, 199);
+            menuStrip1.ForeColor = Color.FromArgb(0, 0, 0);
+        }
+
+        public void DarkTheme()
+        {
+            //Dark Theme
+            BackColor = Color.FromArgb(25, 23, 23);
+            button5.BackColor = Color.FromArgb(45, 41, 41);
+            button5.ForeColor = Color.FromArgb(155, 149, 149);
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.FlatAppearance.BorderColor = Color.FromArgb(35, 33, 33);
+            button5.FlatAppearance.BorderSize = 1;
+            button4.BackColor = Color.FromArgb(45, 41, 41);
+            button4.ForeColor = Color.FromArgb(155, 149, 149);
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.FlatAppearance.BorderColor = Color.FromArgb(35, 33, 33);
+            button4.FlatAppearance.BorderSize = 1;
+            button3.BackColor = Color.FromArgb(45, 41, 41);
+            button3.ForeColor = Color.FromArgb(155, 149, 149);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.FlatAppearance.BorderColor = Color.FromArgb(35, 33, 33);
+            button3.FlatAppearance.BorderSize = 1;
+            button1.BackColor = Color.FromArgb(45, 41, 41);
+            button1.ForeColor = Color.FromArgb(155, 149, 149);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.FlatAppearance.BorderColor = Color.FromArgb(35, 33, 33);
+            button1.FlatAppearance.BorderSize = 1;
+            textBox1.BackColor = Color.FromArgb(35, 33, 33);
+            textBox1.ForeColor = Color.FromArgb(155, 149, 149);
+            menuStrip1.BackColor = Color.FromArgb(10, 10, 10);
+            menuStrip1.ForeColor = Color.FromArgb(155, 149, 149);
+        }
+    }
+}
